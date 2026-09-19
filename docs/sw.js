489891792e8daa82592ac3ebbd1deca1ddc59fe2
Daniel Schroeder <schrsistemas@gmail.com>
2026-09-19 19:32:30 -0300

    feat: add offline service worker

---
+++ docs/sw.js
@@ -0,0 +1,12 @@
+const CACHE='rts-mobile-v1';
+const APP=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(c=>c.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
